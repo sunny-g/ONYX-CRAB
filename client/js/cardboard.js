ae77150b885cfd9489692a96309c69814209e2d0
aggfr12 <avidunn@gmail.com>
2014-12-04 19:50:49 -0800

    fixed cardboard merge issues

--- client/js/cardboard.js
+++ client/js/cardboard.js
@@ -1,84 +1,18 @@
+// Cardboard
+
 function Cardboard() {
 
   this.renderer = new THREE.WebGLRenderer({
     antialias: true
   });
   this.renderer.setSize(window.innerWidth, window.innerHeight);
-var camera, scene, renderer;
-var effect, controls;
-var element, container;
-
-var clock = new THREE.Clock();
-
-function sceneInit() {
-  /********************/
-  /********************/
-  // BOILERPLATE TO RENDER THE ENVIRONMENT
-  // DO NOT TOUCH THIS
-  renderer = new THREE.WebGLRenderer();
-  element = renderer.domElement;
-  container = document.getElementById('stereoView');
-  container.appendChild(element);
-
-  effect = new THREE.StereoEffect(renderer);
-  scene = new THREE.Scene();
-
-  /********************/
-  // UNDERSTAND THIS STUFF BETTER
-  // camera = new THREE.PerspectiveCamera(90, 1, 0.001, 700);
-  camera = new THREE.PerspectiveCamera(45, 650/580, 0.1, 700 );
-  camera.position.set(0, 10, 0);
-  scene.add(camera);
-
-  controls = new THREE.OrbitControls(camera, element);
-  controls.rotateUp(Math.PI / 2);
-  controls.target.set(
-    // camera.position.x + 0.1,
-    camera.position.x,
-    camera.position.y,
-    camera.position.z
-  );
-  controls.noZoom = true;
-  controls.noPan = true;
-
-  var pointLight =
-    new THREE.PointLight(0xFFFFFF);
-
-  // set its position
-  pointLight.position.x = 10;
-  pointLight.position.y = 50;
-  pointLight.position.z = 130;
-
-  // add to the scene
-  scene.add(pointLight);
-
-  function setOrientationControls(e) {
-    if (!e.alpha) {
-      return;
-    }
-
-    controls = new THREE.DeviceOrientationControls(camera, true);
-    controls.connect();
-    controls.update();
-
-    element.addEventListener('click', fullscreen, false);
-    window.removeEventListener('deviceorientation', setOrientationControls);
-  }
-
-  window.addEventListener('deviceorientation', setOrientationControls, true);
-
-  var light = new THREE.HemisphereLight(0x777777, 0x000000, 0.6);
-  scene.add(light);
-  
-  window.addEventListener('resize', resize, false);
-  setTimeout(resize, 1);
-}
 
   this.scene = new THREE.Scene();
   this.effect = new THREE.StereoEffect(this.renderer);
 
   this.camera = new THREE.PerspectiveCamera(
-  90, window.innerWidth / window.innerHeight, 0.001, 700);
+    90, window.innerWidth / window.innerHeight, 0.001, 700
+  );
   this.scene.add(this.camera);
 
   this.controls = new THREE.DeviceOrientationControls(this.camera, true);
