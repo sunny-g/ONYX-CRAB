f7a035eaae7f91168f47420c2d72314af6b8e6dc
owen-d <ow.diehl@gmail.com>
2014-12-06 16:24:24 -0800

    annotated threeJS files and removed lots of commented-out code

--- client/js/3d.js
+++ client/js/3d.js
@@ -2,19 +2,42 @@
 // var filename = 'photos/dolphin.jpg';
 var cardboard;
 
-function setup() {
 
-  cardboard = new Cardboard();
-  imageInit(1,2, cardboard);
-  cardboard.effect.separation = 0.6;
-  cardboard.update = function() {
-    Cardboard.prototype.update.call(this);
-  };
-  document.getElementById('stereoView').appendChild(cardboard.renderer.domElement);
+function imageInit(url, filename, width, height) {
+
+  //currently, we just pass in width/height, but you may want to somehow get image dimensions to pass in as variables.
+  //here we load our picture.
+  var pic = THREE.ImageUtils.loadTexture(url + filename, null, function() {
+    console.log('loading texture');
+  }, function(err) {
+    if (err) {
+      console.log('got an error loading texture');
+    }
+  });
+  //this is our plane object.
+  var planegeometry = new THREE.PlaneBufferGeometry( width, height ); //635, 441 were originial dimensions.
+  //This is the material we use for the plane. We map the image onto it.
+  var planematerial = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+    map: pic
+  });
+  //this meshes the geometry and material together. Then we rotate and add it to our scene.
+  var plane = new THREE.Mesh( planegeometry, planematerial );
+  plane.rotation.y = -Math.PI/2;
+  scene.add( plane );
+
+  var c = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2))
+  //3.87 as a constant is just what I chose. It was reverse engineered from when both camera positions were initiated to -200. Feel free to change it.
+  camera.position.y = -c/3.87;
+  camera.position.x = -c/3.87;
+  // both initiated to -200
 }
 
-function imageInit(filepath, width, height) {
 
+
+
+
+//-----------------------BELOW IS INFORMATION, JUST FOR REFERENCE--------------------
 //  var pic = THREE.ImageUtils.loadTexture(url + filename, null, function() {
 //    console.log('loading texture');
 //  }, function(err) {
@@ -33,76 +56,13 @@ function imageInit(filepath, width, height) {
 //
 //  cardboard.camera.position.y = 441 / 1.5;
 
-  var pic = THREE.ImageUtils.loadTexture(filepath, null, function() {
-    console.log('loading texture');
-  }, function(err) {
-    if (err) {
-      console.log('got an error loading texture');
-    }
-  });
+// function setup() {
 
-  var planegeometry = new THREE.PlaneBufferGeometry( width, height ); //635, 441
-  var planematerial = new THREE.MeshBasicMaterial({
-    color: 0xFFFFFF,
-    map: pic
-  });
-  var plane = new THREE.Mesh( planegeometry, planematerial );
-  plane.rotation.y = -Math.PI/2;
-  scene.add( plane );
-
-  var c = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2))
-  //3.87 as a constant is just what I chose. The magic number, so to speak. Feel free to change it.
-  camera.position.y = -c/3.87;
-  camera.position.x = -c/3.87;
-  // both initiated to -200
-
-//  var light = new THREE.HemisphereLight(0x777777, 0x000000, 0.6);
-//  cardboard.scene.add(light);
-//
-//  var texture = THREE.ImageUtils.loadTexture(
-//    url + 'checker.png'
-//  );
-//  texture.wrapS = THREE.RepeatWrapping;
-//  texture.wrapT = THREE.RepeatWrapping;
-//  texture.repeat = new THREE.Vector2(50, 50);
-//  texture.anisotropy = cardboard.renderer.getMaxAnisotropy();
-//
-//  var material = new THREE.MeshPhongMaterial({
-//    color: 0xffffff,
-//    specular: 0xffffff,
-//    shininess: 20,
-//    shading: THREE.FlatShading,
-//    map: texture
-//  });
-//
-//  var geometry = new THREE.PlaneGeometry(1000, 1000);
-//
-//  var mesh = new THREE.Mesh(geometry, material);
-//  mesh.rotation.x = -Math.PI / 2;
-//  cardboard.scene.add(mesh);
-
-//  var light = new THREE.HemisphereLight(0x777777, 0x000000, 0.6);
-//  scene.add(light);
-//
-//  var texture = THREE.ImageUtils.loadTexture(
-//      url + 'checker.png'
-//  );
-//  texture.wrapS = THREE.RepeatWrapping;
-//  texture.wrapT = THREE.RepeatWrapping;
-//  texture.repeat = new THREE.Vector2(50, 50);
-//  texture.anisotropy = renderer.getMaxAnisotropy();
-//
-//  var material = new THREE.MeshPhongMaterial({
-//    color: 0xffffff,
-//    specular: 0xffffff,
-//    shininess: 20,
-//    shading: THREE.FlatShading,
-//    map: texture
-//  });
-//
-//  var geometry = new THREE.PlaneGeometry(1000, 1000);
-//
-//  var mesh = new THREE.Mesh(geometry, material);
-//  mesh.rotation.x = -Math.PI / 2;
-//  scene.add(mesh);
-}
+//   cardboard = new Cardboard();
+//   imageInit(1,2, cardboard);
+//   cardboard.effect.separation = 0.6;
+//   cardboard.update = function() {
+//     Cardboard.prototype.update.call(this);
+//   };
+//   document.getElementById('stereoView').appendChild(cardboard.renderer.domElement);
+// }
--- client/js/cardboard.js
+++ client/js/cardboard.js
@@ -1,137 +1,31 @@
-//// Cardboard
-//
-//function Cardboard() {
-//
-//  this.renderer = new THREE.WebGLRenderer({
-//    antialias: true
-//  });
-//  this.renderer.setSize(window.innerWidth, window.innerHeight);
-//
-//  this.scene = new THREE.Scene();
-//  this.effect = new THREE.StereoEffect(this.renderer);
-//
-//  this.camera = new THREE.PerspectiveCamera(
-//    90, window.innerWidth / window.innerHeight, 0.001, 700
-//  );
-//  this.scene.add(this.camera);
-//
-//  this.controls = new THREE.DeviceOrientationControls(this.camera, true);
-//
-//  this.orbitControls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
-//  this.orbitControls.noZoom = true;
-//  this.orbitControls.noPan = true;
-//  this.orbitControls.autoRotate = false;
-//
-//  window.addEventListener('resize', this.resize.bind(this), false);
-//
-//  this._initControls = this.initControls.bind(this);
-//  window.addEventListener('deviceorientation', this._initControls, false);
-//
-//  setTimeout(this.resize.bind(this), 0);
-//
-//  // hack for resize when iframe doesn't get a rotation event
-//  // window.addEventListener('message', this.resize.bind(this), false );
-//
-//  this.animate = this.animate.bind(this);
-//  setTimeout(this.play.bind(this), 0);
-//
-//}
-//
-//Cardboard.prototype.initControls = function(event) {
-//  if (event.alpha) {
-//
-//    window.removeEventListener('deviceorientation', this._initControls, false);
-//    this.renderer.domElement.addEventListener('click', this.fullscreen.bind(this), false);
-//
-//    this.orbitControls.enabled = false;
-//
-//    this.controls.connect();
-//    this.controls.update();
-//  }
-//};
-//
-//Cardboard.prototype.animate = function() {
-//  if (!this._playing)
-//    return;
-//  requestAnimationFrame(this.animate);
-//  this.update();
-//  this.render();
-//};
-//
-//Cardboard.prototype.pause = function() {
-//  this._playing = false;
-//};
-//
-//Cardboard.prototype.play = function() {
-//  if (this._playing) return;
-//  this._playing = true;
-//  this.animate();
-//};
-//
-//Cardboard.prototype.update = function() {
-//  // hack to resize if width and height change
-//  if (this.width !== window.innerWidth || this.height !== window.innerHeight) {
-//    this.resize();
-//  }
-//  this.camera.updateProjectionMatrix();
-//  if (this.controls.freeze === false) {
-//    this.controls.update();
-//  } else {
-//    this.orbitControls.update();
-//  }
-//};
-//
-//Cardboard.prototype.render = function() {
-//  //if (this.controls.freeze === false) {
-//  this.effect.render(this.scene, this.camera);
-//  //} else {
-//  //this.renderer.render(this.scene, this.camera);
-//  //}
-//};
-//
-//Cardboard.prototype.fullscreen = function() {
-//  if (this.renderer.domElement.requestFullscreen) {
-//    this.renderer.domElement.requestFullscreen();
-//  } else if (this.renderer.domElement.msRequestFullscreen) {
-//    this.renderer.domElement.msRequestFullscreen();
-//  } else if (this.renderer.domElement.mozRequestFullScreen) {
-//    this.renderer.domElement.mozRequestFullScreen();
-//  } else if (this.renderer.domElement.webkitRequestFullscreen) {
-//    this.renderer.domElement.webkitRequestFullscreen();
-//  }
-//};
-//
-//Cardboard.prototype.resize = function() {
-//  this.width = window.innerWidth;
-//  this.height = window.innerHeight;
-//  this.camera.aspect = this.width / this.height;
-//  this.camera.updateProjectionMatrix();
-//  this.renderer.setSize(this.width, this.height);
-//  this.effect.setSize(this.width, this.height);
-//};
+  
 
 var camera, scene, renderer;
 var effect, controls;
 var element, container;
 
+//clock tracks time
 var clock = new THREE.Clock();
 
 
 function init() {
+  //Be wary of rabbit holes from here on out. Below we set up our environment.
   renderer = new THREE.WebGLRenderer();
   element = renderer.domElement;
   container = document.getElementById('stereoView');
   container.appendChild(element);
 
+  //google stuff
   effect = new THREE.StereoEffect(renderer);
-
+  //scene is our environment where we place things.
   scene = new THREE.Scene();
-
+  //This is our camera. We create it and then add it to the scene.
   camera = new THREE.PerspectiveCamera(90, 1, 0.001, 700);
   camera.position.set(0, 10, 0);
   scene.add(camera);
-
+  //more google stuff.
   controls = new THREE.OrbitControls(camera, element);
+  //small adjustments here for visual benefits
   controls.rotateUp(Math.PI / 4);
   controls.target.set(
     camera.position.x + 0.1,
@@ -145,6 +39,8 @@ function init() {
 //  controls.noRotate = true;
   controls.autoRotate = false;
 
+
+  //Thank you google. I haven't touched anything below here.
   function setOrientationControls(e) {
     if (!e.alpha) {
       return;
